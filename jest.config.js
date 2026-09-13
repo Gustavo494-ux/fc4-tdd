3da83d525cc8884432fcac32d33f87d8197cab98
Gustavo <gustavogama494@gmail.com>
2026-09-13 14:14:50 -0300

    chore: aprimora configuração TypeScript e autocomplete do projeto

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
   moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
+  },
 };
