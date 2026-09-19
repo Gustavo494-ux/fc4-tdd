b7ce3a3c781804a14132484ff1976585ec41ed6a
Gustavo <gustavogama494@gmail.com>
2026-09-19 09:27:57 -0300

    test: adiciona testes para as regras de reembolso da RefundRuleFactory

---
+++ src/domain/cancelation/refund_rule_factory.test.ts
@@ -0,0 +1,18 @@
+import { FullRefund } from "./full_refund";
+import { NoRefund } from "./no_refund copy";
+import { PartialRefund } from "./partial_refund";
+import { RefundRuleFactory } from "./refund_rule_factory";
+
+describe("RefundRuleFactory", () => {
+    it("deve retornar FullRefund quando a reserva for cancelada com mais de 7 dias de antecedência", () => {
+        expect(RefundRuleFactory.getRefundRule(8)).toBeInstanceOf(FullRefund);
+    })
+    it("deve retornar PartialRefund quando a reserva for cancelada entre 1 e 7 dias de antecedência", () => {
+        expect(RefundRuleFactory.getRefundRule(1)).toBeInstanceOf(PartialRefund);
+        expect(RefundRuleFactory.getRefundRule(7)).toBeInstanceOf(PartialRefund);
+    });
+    it("deve retornar NoRefund quando a reserva for cancelada com menos de 1 dia de antecedência", () => {
+        expect(RefundRuleFactory.getRefundRule(0)).toBeInstanceOf(NoRefund);
+        expect(RefundRuleFactory.getRefundRule(-1)).toBeInstanceOf(NoRefund);
+    });
+});
